44f149f889b1001c9850b7fcc252da7a903c6fda
Dung Nguyen <nguyenmanhdungtk@gmail.com>
2018-03-13 17:04:16 +0700

    update contents

--- 1-1/1.js
+++ 1-1/1.js
@@ -1,20 +1,22 @@
 var input = document.getElementsByTagName("input")[0];
 var addBtn = document.getElementById("add-btn");
-var toDoElement = document.getElementsByClassName("done")[0];
+var toDoElement = document.querySelectorAll(".row.mx-0")[0];
 var newElement;
 
 main();
 
 function main() {
-    // Add add-button listener
+    // Add add-button listener (Add item to the list)
     addBtn.addEventListener("click", function () {
         if (input !== "") {
             newElement = toDoElement.cloneNode(true);
+            newElement.classList.remove("d-none");
             newElement.classList.remove("done");
             newElement.children[0].classList.add("d-none");
             newElement.children[1].classList.add("offset-1");
             newElement.children[1].children[0].textContent = input.value;
             toDoElement.parentNode.appendChild(newElement);
+            addElementListener(newElement);
         }
     });
     // add Input listener
@@ -22,5 +24,14 @@ function main() {
         if (event.keyCode === 13) {
             addBtn.click();
         }
+    });
+}
+
+// add listener for new element
+function addElementListener(element) {
+    element.addEventListener("click", function () {
+        this.classList.toggle("done");
+        this.children[0].classList.toggle("d-none");
+        this.children[1].classList.toggle("offset-1");
     })
 }
