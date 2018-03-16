322048f505f313546b7d7e204736311cea302c5c
Dung Nguyen <nguyenmanhdungtk@gmail.com>
2018-03-17 02:17:33 +0700

    update contents

---
+++ 2-1/app.js
@@ -0,0 +1,48 @@
+var input = document.getElementsByTagName("input")[0];
+var addBtn = document.getElementById("add-btn");
+var patternElement = document.querySelectorAll(".row.mx-0")[0];
+var newElement;
+
+var counter = 0;
+
+main();
+
+function main() {
+    // Add add-button listener (Add item to the list)
+    addBtn.addEventListener("click", function () {
+        if (input.value !== "") {
+            newElement = patternElement.cloneNode(true);
+            newElement.classList.remove("d-none");
+            newElement.classList.remove("done");
+            newElement.children[0].classList.add("d-none");
+            newElement.children[1].classList.add("offset-1");
+            newElement.children[1].children[0].textContent = input.value;
+            patternElement.parentNode.appendChild(newElement);
+            addElementListener(newElement);
+            addDeleteBtnListener(newElement.children[2]);
+            input.value = "";
+        }
+    });
+    // add input listener
+    input.addEventListener("keyup", function (event) {
+        if (event.keyCode === 13) {
+            addBtn.click();
+        }
+    });
+}
+
+// add "done" class for new element
+function addElementListener(element) {
+    element.addEventListener("click", function () {
+        this.classList.toggle("done");
+        this.children[0].classList.toggle("d-none");
+        this.children[1].classList.toggle("offset-1");
+    })
+}
+
+// add "delete" function for new element
+function addDeleteBtnListener(deleteBtn) {
+    deleteBtn.addEventListener("click", function () {
+        this.parentElement.remove();
+    });
+}
